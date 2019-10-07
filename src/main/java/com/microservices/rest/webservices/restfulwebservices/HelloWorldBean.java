9fe79d41d3ab1113c7bef27ab2e17d4db3914e98
nikhil <NIKHIL.GUPTA@PEARSON.COM>
2019-10-08 00:03:22 +0530

    enhancing hello world controller with bean.

---
+++ src/main/java/com/microservices/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,22 @@
+package com.microservices.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+  private String message;
+
+  public HelloWorldBean(String messge) {
+    this.message = messge;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Hello world bean [message=%s]",message);
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+}
