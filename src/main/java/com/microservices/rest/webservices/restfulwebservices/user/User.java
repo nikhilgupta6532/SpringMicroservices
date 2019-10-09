256886f89af5457c025b16339bb6b1ff4f708662
nikhil <NIKHIL.GUPTA@PEARSON.COM>
2019-10-09 19:28:22 +0530

    introudced swagger.

--- src/main/java/com/microservices/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/microservices/rest/webservices/restfulwebservices/user/User.java
@@ -1,16 +1,22 @@
 package com.microservices.rest.webservices.restfulwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "All details about the user.")
 public class User {
   private Integer id;
 
   @Size(min = 2,message = "Name should have atleast 2 characters")
+  @ApiModelProperty(notes = "Names should have atleast 2 characters")
   private String name;
 
   @Past
+  @ApiModelProperty(notes = "Birth Date should be in the past")
   private Date birthDate;
 
   protected User() { }
